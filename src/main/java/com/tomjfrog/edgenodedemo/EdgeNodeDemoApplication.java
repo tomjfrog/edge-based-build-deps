893ec35cdc3aa6979c3c2594fad10e631fc22a7d
tomjfrog <tomj@jfrog.com>
2024-07-03 13:46:24 -0500

    Initial Commit

---
+++ src/main/java/com/tomjfrog/edgenodedemo/EdgeNodeDemoApplication.java
@@ -0,0 +1,13 @@
+package com.tomjfrog.edgenodedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdgeNodeDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EdgeNodeDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tomjfrog/edgenodedemo/EdgeNodeDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tomjfrog.edgenodedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdgeNodeDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
